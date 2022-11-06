aee5491f227436f29199390dd1b0f94cbcf9b973
OmarXtream <84891971+Rahaf-Abbas@users.noreply.github.com>
2022-11-06 23:38:26 +0300

    Test Case

---
+++ Algo/DataGenerator.py
@@ -0,0 +1,22 @@
+# Data Generator
+
+#Book 1 ( rating 4 and 5 by 200 users)
+for i in range(1,100):
+    print("Book 1," , "4", ",",i)
+for i in range(101,200):
+    print("Book 1," , "5", ",",i)
+
+#Book 2 ( rating 4 and 5 by 200 users)
+for i in range(1,100):
+    print("Book 2," , "4", ",",i)
+for i in range(101,200):
+    print("Book 2," , "5", ",",i)
+
+#Book 3 ( rating 2 by 50 users)
+for i in range(1,50):
+    print("Book 3," , "2", ",",i)
+
+
+#Book 4 ( rating 3 by 30 users)
+for i in range(1,30):
+    print("Book 4," , "2", ",",i)
